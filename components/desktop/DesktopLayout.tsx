5fbcfde6ed06e53def25f16852a0f87307a7e373
Kaz Trigger <k.a52@icloud.com>
2025-11-12 16:05:51 +0900

    🐛 useEffectのimport漏れを修正したで
    ダークモード初期化でuseEffect使ってるのにimportしてなかった
    リファクタリング時の見落としを修正

--- components/desktop/DesktopLayout.tsx
+++ components/desktop/DesktopLayout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect } from 'react';
 import { Rnd } from 'react-rnd';
 import { useDesktopStore, type App } from '@/store/desktopStore';
 import { useIsMobile } from '@/hooks/useIsMobile';
